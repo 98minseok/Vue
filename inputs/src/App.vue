c4857468970086ef74248d3e5568b071941b92c7
SI3 KimMinseok <als981209@gmail.com>
2025-01-07 10:37:22 +0900

    input v-model 실전예제

--- inputs/src/App.vue
+++ inputs/src/App.vue
@@ -21,6 +21,18 @@
     </option>
   </select>
   <br/>
+  <label>날짜</label>
+  <input type ="date" v-model ="result.date">
+  <br/>
+  <label>시간</label>
+  <input type ="time" v-model ="result.time">
+  <br/>
+  <label>색깔</label>
+  <input type ="color" v-model ="result.color">
+  <br/>
+  <label>범위값</label>
+  <input type ="range" :min ="minValue" :max ="maxValue" v-model ="result.rangeValue">
+  <br/>
   <div>
     <h3>결과</h3>
     <hr/>
@@ -39,18 +51,29 @@ export default{
     const country = ['한국']
     const country_options =['한국','미국','중국']
     const hobby_options = ['스포츠','그림','음악','코딩']
-
+    const date = ""
+    const time = ""
+    const color = ""
+    const rangeValue = ""
+    const minValue = 0
+    const maxValue = 100
     const result = reactive({
       name,
       gender,
       hobby,
-      country
+      country,
+      date,
+      time,
+      color,
+      rangeValue
     })
 
     return{
       result,
       country_options,
-      hobby_options
+      hobby_options,
+      minValue,
+      maxValue
     }
   }
 }
