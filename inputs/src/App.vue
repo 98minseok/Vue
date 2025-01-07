8d332816d1ca0ab6ec9560ff6e5bbd58e09ccda2
SI3 KimMinseok <als981209@gmail.com>
2025-01-07 10:33:03 +0900

    Input v-model 복습습

---
+++ inputs/src/App.vue
@@ -0,0 +1,61 @@
+
+
+<template>
+  <label>이름 : </label>
+  <input v-model="result.name" type ="text" placeholder="이름을 입력하세요"><br/>
+  <label>성별 : </label>
+  <label>남성</label>
+  <input type ="radio" name = "gender" v-model="result.gender" value ="male">
+  <label>여성</label>
+  <input type ="radio" name = "gender" v-model="result.gender" value ="female"><br/>
+  <label>취미</label>
+  <span v-for="hobby in hobby_options" :key ="hobby">
+    <label>{{ hobby }}</label>
+    <input type ="checkbox" v-model = "result.hobby" :value = hobby>
+  </span>
+  <br/>
+  <label>국가</label>
+  <select v-model="result.country">
+    <option v-for="country in country_options" :key="country">
+      {{ country }}
+    </option>
+  </select>
+  <br/>
+  <div>
+    <h3>결과</h3>
+    <hr/>
+    {{ result }}
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default{
+  setup(){
+    const name = ""
+    const gender = "male"
+    const hobby = ['스포츠']
+    const country = ['한국']
+    const country_options =['한국','미국','중국']
+    const hobby_options = ['스포츠','그림','음악','코딩']
+
+    const result = reactive({
+      name,
+      gender,
+      hobby,
+      country
+    })
+
+    return{
+      result,
+      country_options,
+      hobby_options
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
